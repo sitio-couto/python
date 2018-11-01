aa5b1036537b634a405d7e13ff6cad4d7fdbefe0
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-11-01 15:34:20 -0300

    test

---
+++ test11.py
@@ -0,0 +1,28 @@
+import time
+
+class logstr(object):
+
+    def __init__(self, f):
+        self.log = ""
+        self.f = f
+
+    def __call__(self, *args):
+        ret = self.f(*args)
+        self.log += time.asctime(time.localtime())
+        self.log += " Entrada: " + str(args) + " Return: " + str(ret) + "\n"
+        return ret
+#
+# def logstr(function):
+#     log = "\n"
+#
+#     def wrapper(*args):
+#         ret = function(*args)
+#         log += time.asctime(time.localtime())
+#         # log += " Entrada: " + str(args) + " Return: " + str(ret) + "\n"
+#         return ret
+#
+#     return wrapper
+
+@logstr
+def add(a, b):
+    return a+b
