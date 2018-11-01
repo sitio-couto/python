53cd4e47c3ff4da041fd258b72e0d302ff81d047
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-11-01 15:52:38 -0300

    funcao inves de classe

--- test11.py
+++ test11.py
@@ -1,27 +1,15 @@
 import time
+import functools
 
-class logstr(object):
-
-    def __init__(self, f):
-        self.log = ""
-        self.f = f
-
-    def __call__(self, *args):
-        ret = self.f(*args)
-        self.log += time.asctime(time.localtime())
-        self.log += " entrada: " + str(args) + " saida: " + str(ret) + "\n"
+def logstr(function):
+    @functools.wraps(function)
+    def wrapper(*args):
+        ret = function(*args)
+        wrapper.log += time.asctime(time.localtime())
+        wrapper.log += " entrada: " + str(args) + " saida: " + str(ret) + "\n"
         return ret
-#
-# def logstr(function):
-#     log = "\n"
-#
-#     def wrapper(*args):
-#         ret = function(*args)
-#         log += time.asctime(time.localtime())
-#         # log += " Entrada: " + str(args) + " Return: " + str(ret) + "\n"
-#         return ret
-#
-#     return wrapper
+    wrapper.log = ""
+    return wrapper
 
 @logstr
 def add(a, b):
